069b6c31a725ac431eb77dda3a2ebc19bf305936
Audrey <audreyakosuaasaankrumah@gmail.com>
2024-01-27 00:26:17 -0500

    update flask

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def precipatation ():
         #create session link
     session = Session(engine)
     #date 
-    dMost_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+    Most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
     # Calculate the date one year from the last date in data set.
     Year_Precipitation = (dt.date(2017,8,23) - dt.timedelta(days=365)).strftime('%Y-%m-%d')
     Year_from_date = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= Year_Precipitation).all()
@@ -73,6 +73,7 @@ def stations ():
     order_by(func.count(Measurement.station).desc()).all()
         
     session.close()
+
     stat=list(np.ravel(Active_Stations))
     print("List of Station ID and name")
     return jsonify(stat)
@@ -81,19 +82,34 @@ def stations ():
 def tobs ():
     session = Session(engine)
     #date 
-    date=session.query(Measurement.date).\
-    order_by(Measurement.date.desc()).first().date
-    # Calculate the date one year from the last date in data set.
-    year_last=dt.datetime.strptime(date,'%Y-%m-%d')-dt.timedelta(days=365)
-    #last year temp
-    temp_station=session.query(Measurement.date,Measurement.tobs).\
-    filter(Measurement.date>=year_last).\
-    filter(Measurement.station == "USC00519281").\
-    order_by(Measurement.date).all()
+    Most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+
+    # Design a query to calculate the total number of stations in the dataset
+    session.query(func.count(Station.station)).all()
+
+    Active_Stations = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+
+    # Using the most active station id from the previous query, calculate the lowest, highest, and average temperature.
+    Most_Actve_Station_Id = 'USC00519281'
+
+    lowest_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
+    highest_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
+    avgerage_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
     
-    session.close()
-    temp=list(np.ravel(temp_station))
-    return jsonify(temp)  
+    # Using the most active station id, Query the last 12 months of temperature observation data for this station and plot the results as a histogram
+    Year_Precipitation = (dt.date(2017,8,23) - dt.timedelta(days=365)).strftime('%Y-%m-%d')
+    Annual_data = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station == Most_Actve_Station_Id).\
+    filter(Measurement.date >= Year_Precipitation).all()
+    Annual_Data_df = pd.DataFrame(Annual_data, columns=['date', 'temperature'])
+    temperature =list(np.ravel(Most_Actve_Station_Id))
+
+    return jsonify(temperature)  
+    
+session.close()
+
 # Sepecified date temperatures
 @app.route("/api/v1.0/<start_date>")
 def start_date (start_date):
@@ -103,13 +119,16 @@ def start_date (start_date):
     #create session
     session=Session(engine)
     
-    # date filter
-    Temperature=session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
-    filter(Measurement.date>=start_date).all()
+    # Using the most active station id from the previous query, calculate the lowest, highest, and average temperature.
+    Most_Actve_Station_Id = 'USC00519281'
+    lowest_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
+    highest_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
+    avgerage_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.station == Most_Actve_Station_Id).one()[0]
+    print(f"lowest_temp = {lowest_temp}, highest_temp = {highest_temp}, average_temp = {avgerage_temp}")
     
     session.close()
-    temp_st=list(np.ravel(Temperature))
-    return jsonify(temp_st)  
+    Temperature=list(np.ravel(Most_Actve_Station_Id))
+    return jsonify(Temperature)  
 #within dates (start and end dates)   
 @app.route("/api/v1.0/<start_date>/<end_date>")
 def st_ed_date (start_date,end_date):
